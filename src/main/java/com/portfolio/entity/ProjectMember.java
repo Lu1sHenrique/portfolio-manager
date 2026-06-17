02fc309c0e71f5de179c37da5e23cc566129e699
lu1sHenrique <lhenrique.lh000@gmail.com>
2026-06-17 13:57:33 -0300

    feat: criando entitys

---
+++ src/main/java/com/portfolio/entity/ProjectMember.java
@@ -0,0 +1,47 @@
+package com.portfolio.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "project_members", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"project_id", "member_id"})
+})
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "allocation_date", nullable = false)
+    @Builder.Default
+    private LocalDate allocationDate = LocalDate.now();
+}
